d112b4bb5ab044e1451a9eed5e455fe60ff64895
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-26 10:10:51 +0100

    form in not yet usable

--- src/pages/PageLogin.tsx
+++ src/pages/PageLogin.tsx
@@ -1,70 +1,42 @@
 import { useContext, useRef } from 'react';
 import { AppContext } from '../AppContext';
 import { Helmet } from 'react-helmet';
-import { useNavigate } from 'react-router-dom';
 
 export const PageLogin = () => {
 	const {
 		appTitle,
-		loginAsAdmin,
-		password,
-		setPassword,
-		adminIsLoggedIn,
-		logoutAsAdmin,
 	} = useContext(AppContext);
-	const navigate = useNavigate();
-	const passwordRef = useRef() as React.RefObject<HTMLInputElement>;
-
-	const loginAndReact = () => {
-		loginAsAdmin(
-			() => {
-				navigate('/');
-			},
-			() => {
-				if (passwordRef.current !== null) {
-					passwordRef.current.focus();
-				}
-			}
-		);
-	};
-
-	const handleKeyDown = (e: any) => {
-		if (e.key === 'Enter') {
-			loginAndReact();
-		}
-	};
 
 	return (
 		<div className="page pageLogin">
 			<Helmet>
 				<title>{appTitle} - Login</title>
 			</Helmet>
-			{adminIsLoggedIn ? (
-				<p>
-					<button className="logout" onClick={logoutAsAdmin}>
-						Logout
-					</button>
-				</p>
-			) : (
-				<p>
-					Identify as admin:{' '}
-					<input
-						type="password"
-						ref={passwordRef}
-						autoFocus
-						value={password}
-						onKeyDown={handleKeyDown}
-						onChange={(e) => setPassword(e.target.value)}
-					/>{' '}
-					<button
-						disabled={password.trim() === ''}
-						onClick={loginAndReact}
-						type="button"
-					>
-						Login
-					</button>
-				</p>
-			)}
+					<form>
+						<fieldset>
+							<legend>Please provide your credentials</legend>
+							<div className="row">
+								<label>Username</label>
+								<div>
+									<input autoFocus type="text" />
+								</div>
+							</div>
+
+							<div className="row">
+								<label>Password</label>
+								<div>
+									<input type="password" />
+								</div>
+							</div>
+
+							<div className="buttonArea">
+								<div className="message">
+									This is a test message.
+								</div>
+								<button type="button">Submit</button>
+							</div>
+						</fieldset>
+					</form>
 		</div>
 	);
 };
